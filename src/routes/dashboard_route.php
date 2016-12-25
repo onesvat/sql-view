940ed76ca04337e497ce684284ff80840ba1c2f4
Onur Neşvat <onurnesvat@gmail.com>
2016-12-25 16:53:05 +0300

    Add something

--- src/routes/dashboard_route.php
+++ src/routes/dashboard_route.php
@@ -4,5 +4,6 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 $app->get('/dashboard', function (Request $request, Response $response, $args) use ($app) {
+
     return $this->view->render($response, 'dashboard.html.twig', array_merge($app->extra, $args));
 })->add($user_auth);
--- src/routes/database_route.php
+++ src/routes/database_route.php
@@ -5,21 +5,27 @@ use Slim\Http\Response;
 
 $app->get('/database', function (Request $request, Response $response, $args) use ($app) {
 
-});
+    return $this->view->render($response, 'database.html.twig', array_merge($app->extra, $args));
+})->add($user_auth);
 
 $app->get('/database/new', function (Request $request, Response $response, $args) use ($app) {
 
-});
+    return $this->view->render($response, 'database_new.html.twig', array_merge($app->extra, $args));
+})->add($user_auth);
 
 $app->post('/database/new', function (Request $request, Response $response, $args) use ($app) {
 
-});
+})->add($user_auth);
 
 $app->get('/database/edit/:dtb_id', function (Request $request, Response $response, $args) use ($app) {
 
-});
+    return $this->view->render($response, 'database_edit.html.twig', array_merge($app->extra, $args));
+})->add($user_auth);
 
 $app->post('/database/edit/:dtb_id', function (Request $request, Response $response, $args) use ($app) {
 
-});
+})->add($user_auth);
 
+$app->get('/database/delete/:dtb_id', function (Request $request, Response $response, $args) use ($app) {
+
+})->add($user_auth);
