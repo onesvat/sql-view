76a32de60e1d564188f8445538affdb77936088d
Onur Neşvat <onurnesvat@gmail.com>
2016-12-27 14:42:26 +0300

    Add something

--- src/routes/query_route.php
+++ src/routes/query_route.php
@@ -5,7 +5,34 @@ use Slim\Http\Response;
 
 $app->get('/query', function (Request $request, Response $response, $args) use ($app) {
 
+    $stmt = $app->connection->prepare("SELECT TABLE_NAME table_name, COLUMN_NAME column_name, COLUMN_TYPE column_type, DATA_TYPE column_data_type FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='adjo-panel'");
+    $stmt->execute();
+    $fields = $stmt->fetchAll();
 
+    $tables = [];
+    foreach ($fields as $field) {
+        if (!array_key_exists($field['table_name'], $tables)) {
+            $tables[$field['table_name']] = ['table_name' => $field['table_name'], 'columns' => []];
+        }
+
+        $tables[$field['table_name']]['columns'][] = ['column_name' => $field['column_name'], 'column_type' => $field['column_type'], 'column_data_type' => $field['column_data_type']];
+    }
+
+
+    $tree = [];
+
+    foreach ($tables as $table) {
+        $columns = [];
+
+        foreach ($table['columns'] as $column) {
+            $columns[] = ["text" => $column['column_name'] . " - <i>" . $column['column_data_type'] . "</i>"];
+        }
+
+        $tree[] = ["text" => $table['table_name'], 'nodes' => $columns, 'state' => ['expanded' => false]];
+    }
+
+    $args['tables'] = $tables;
+    $args['tree'] = json_encode($tree);
 
     return $this->view->render($response, 'query.html.twig', array_merge($app->extra, $args));
 })->add($user_auth);
