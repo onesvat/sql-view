c4a493128d3b1acf4c3ff2bd1022ed24b9943161
kemal <kemal.21184@hotmail.com>
2016-12-31 17:12:52 +0300

    Some Improvements

--- src/routes/users_route.php
+++ src/routes/users_route.php
@@ -105,10 +105,7 @@ $app->get('/users/tables/give_permission/{usr_id}/{connection_id}/{table_name}',
 })->add($admin_auth);
 
 
-
 $app->get('/users/tables/remove_permission/{usr_id}/{connection_id}/{table_name}', function (Request $request, Response $response, $args) use ($app) {
-
-    R::debug();
 
 
     R::exec("DELETE FROM permissions WHERE prm_connection_id= :prm_connection_id AND prm_usr_id= :prm_usr_id AND prm_table_name= :prm_table_name", ['prm_connection_id' => $args['connection_id'], 'prm_usr_id' => $args['usr_id'], 'prm_table_name' => $args['table_name']]);
@@ -118,24 +115,55 @@ $app->get('/users/tables/remove_permission/{usr_id}/{connection_id}/{table_name}
 })->add($admin_auth);
 
 
-$app->get('/users/permissions/fields/{usr_id}/{connection_id}', function (Request $request, Response $response, $args) use ($app) {
-
-    $args['connections'] = R::getAll("SELECT * FROM connections");
+$app->get('/users/permissions/fields/{usr_id}/{connection_id}/{table_name}', function (Request $request, Response $response, $args) use ($app) {
 
 
-    return $this->view->render($response, 'user_permissions.html.twig', array_merge($app->extra, $args));
+    $active_connection = R::getRow("SELECT * FROM connections WHERE cnn_id = :cnn_id", ['cnn_id' => $args['connection_id']]);
+    $active_connection['connection'] = json_decode($active_connection['cnn_connection'], true);
+
+    try {
+        $app->connection = new PDO(
+            "mysql:host=" . $active_connection['connection']['cnn_host'] . ";dbname=" . $active_connection['connection']['cnn_database'],
+            $active_connection['connection']['cnn_username'],
+            $active_connection['connection']['cnn_password'],
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+            ]
+        );
+
+        $stmt = $app->connection->prepare("DESCRIBE {$args['table_name']}");
+        $stmt->execute();
+
+        $args['fields'] = $stmt->fetchAll(PDO::FETCH_COLUMN);
+    } catch (Exception $e) {
+        echo 'Connection failed' . $e->getMessage();
+        die;
+    }
+
+
+    foreach ($args['fields'] as $key => $field_name) {
+        $table_permission = R::getRow("SELECT * FROM permissions WHERE prm_usr_id = :prm_usr_id AND prm_connection_id = :prm_connection_id AND prm_table_name = :prm_table_name AND prm_field_name = :prm_field_name", ['prm_usr_id' => $args['usr_id'], 'prm_connection_id' => $args['connection_id'], 'prm_table_name' => $args['table_name'], 'prm_field_name' => $field_name]);
+        if (empty($table_permission)) {
+            $args['field_permission'][$key]['give_permission_button'] = 'enabled';
+        } else {
+            $args['field_permission'][$key]['give_permission_button'] = 'disabled';
+        }
+    }
+
+
+    return $this->view->render($response, 'user_field_permissions.html.twig', array_merge($app->extra, $args));
 })->add($admin_auth);
 
 
-$app->get('/users/fields/give_permission/{usr_id}', function (Request $request, Response $response, $args) use ($app) {
+$app->get('/users/fields/give_permission/{usr_id}/{connection_id}/{table_name}/{field_name}', function (Request $request, Response $response, $args) use ($app) {
 
-    $permissions = $request->getParam('permissions');
-//    foreach ($permissions as $key => $permission) {
-//        R::exec("DELETE FROM permissions WHERE prm_usr_id = :prm_usr_id", ['prm_usr_id' => $args['usr_id']]);
-//        R::exec("INSERT INTO permissions (prm_id,prm_usr_id) VALUES (:prm_id,:prm_usr_id)", ['prm_id' => $key, 'prm_usr_id' => $args['usr_id']]);
-//    }
 
-    return $response->withRedirect('/users');
+    R::exec("INSERT INTO permissions(prm_connection_id,prm_usr_id,prm_table_name,prm_field_name) VALUES(:prm_connection_id,:prm_usr_id,:prm_table_name,:prm_field_name)", ['prm_connection_id' => $args['connection_id'], 'prm_usr_id' => $args['usr_id'], 'prm_table_name' => $args['table_name'], 'prm_field_name' => $args['field_name']]);
+
+
+    return $response->withRedirect('/users/permissions/tables/' . $args['usr_id'] . '/' . $args['connection_id']);
+
 })->add($admin_auth);
 
 
