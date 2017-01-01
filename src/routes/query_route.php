b6bfd29871c342640c15ccaf0cd1b32b14cd1ac2
Onur Neşvat <onurnesvat@gmail.com>
2017-01-02 02:04:27 +0300

    Some changes

--- src/routes/query_route.php
+++ src/routes/query_route.php
@@ -38,6 +38,52 @@ $app->post('/query/run', function (Request $request, Response $response, $args) 
     return $response->withJson($result, 200);
 })->add($user_auth);
 
+$app->post('/query/run/gui', function (Request $request, Response $response, $args) use ($app) {
+    $tables = $request->getParam('tables');
+    $fields = $request->getParam('fields');
+    $builder = $request->getParam('builder');
+
+    $query_string = "SELECT " . implode(",", $fields) . " FROM " . implode(',', $tables);
+
+    if($builder['sql']) {
+        $query_string .= " WHERE " . $builder['sql'];
+    }
+
+    $cache = $request->getParam('cache');
+
+    $query = new Query($query_string, new Connection($app->extra['active_connection']), $app->extra['user']);
+    $result = $query->getArray($cache);
+    return $response->withJson($result, 200);
+})->add($user_auth);
+
+
+$app->post('/query/get_fields', function (Request $request, Response $response, $args) use ($app) {
+    $selected_tables = $request->getParam('tables');
+
+    if (count($selected_tables) == 0)
+        return $response->withJson(['data' => [], 'filter' => []]);
+
+
+    $data = [];
+    $filter = [];
+
+    $connection = new Connection($app->extra['active_connection'], $app->extra['user']);
+
+    $tables = $connection->getFields();
+
+    foreach ($tables as $table) {
+        foreach ($table['columns'] as $column) {
+            if (in_array($table['table_name'], $selected_tables)) {
+                $data[] = ["id" => $table['table_name'] . "." . $column['column_name'], "text" => $table['table_name'] . "." . $column['column_name']];
+                $filter[] = ['id' => $table['table_name'] . "." . $column['column_name'], 'label' => $table['table_name'] . "." . $column['column_name']];
+            }
+        }
+    }
+
+
+    return $response->withJson(['data' => $data, 'filter' => $filter]);
+})->add($user_auth);
+
 $app->get('/query/{query_hash}.{download_type}', function (Request $request, Response $response, $args) use ($app) {
 
     $_query = R::getRow("SELECT * FROM queries WHERE que_hash = :que_hash", ['que_hash' => $args['query_hash']]);
